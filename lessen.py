349b27e3dfe1993f6d8c0d2a0d1c70e9918a0c3b
dadandr <146108340+dadandr@users.noreply.github.com>
2023-10-09 16:53:19 +0400

    Add files via upload

---
+++ lessen.py
@@ -0,0 +1,16 @@
+user_name = input("რა გქვია:")
+user_surname =  input("რა არის შენი გვარი:")
+user_age = input("რა ასაკის ხარ:")
+user_pets_name = input("რა ქვია თქვენს შინაურ ცხოველებს:")
+user_father_name = input("რა ქვია მამაშენს:")
+user_mother_name = input("რა ქვია დედაშენს:" )
+
+print("როგორც მომხმარებელმა თქვა, ის არის", user_name)
+print("როგორც მომხმარებელმა თქვა, მისი გვარია", user_surname)
+print("როგორც მომხმარებელმა თქვა, ის არის", user_age)
+print("როგორც მომხმარებელმა თქვა, მისი ცხოველები არიან", user_pets_name)
+print("როგორც მომხმარებელმა თქვა, მისი მამა არის", user_father_name)
+print("როგორც მომხმარებელმა თქვა, მისი დედა არის", user_mother_name)
+
+
+print("შენ იქნები", int(user_age) + 15, "წლის 15 წლის შემდეგ")
